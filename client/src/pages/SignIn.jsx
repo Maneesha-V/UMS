519ff4a1e6a4901c5f8168ce38f8ed21af1f5781
Maneesha V <manu.maneeshav@gmail.com>
2025-02-01 00:05:48 +0530

    Add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,19 +1,20 @@
 import { Link, useNavigate } from "react-router-dom"
 import React, { useState } from 'react'
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice.js"
+import { useDispatch, useSelector } from "react-redux"
 
 const Signin = () => {
   const [formData,setFormData] = useState({})
-  const [loading,setLoading] = useState(false)
-  const [error,setError] = useState(false)
+  const { loading,error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({...formData,[e.target.id]: e.target.value})
   }
-  const handleSubmiit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    try{
-      setLoading(true)
-      setError(false)
+    dispatch(signInStart());
+    try{  
       const response = await fetch('/api/auth/signin',{
         method: 'POST',
         headers: {
@@ -22,24 +23,23 @@ const Signin = () => {
         body: JSON.stringify(formData),
       })
       const data = await response.json();
-      setLoading(false)
       if(data.success === false){
-        setError(true);
+        dispatch(signInFailure(data));
         return;
       }
+      dispatch(signInSuccess(data));
       navigate('/')
     }catch(err){
-      setLoading(false);
-      setError(true);
+      dispatch(signInFailure(err));
     }
   }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='font-bold text-3xl text-center my-7'>Sign In</h1>
-      <form action="" className='flex flex-col gap-4' onSubmit={handleSubmiit}>
-        <input type="text" placeholder='Email' id='email' onChange={handleChange}
+      <form action="" className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        <input type="email" placeholder='Email' id='email' onChange={handleChange}
         className='bg-slate-100 p-3 rounded-lg text-lg' />
-        <input type="text" placeholder='Password' id='password' onChange={handleChange}
+        <input type="password" placeholder='Password' id='password' onChange={handleChange}
         className='bg-slate-100 p-3 rounded-lg text-lg' />
         <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95
         disabled:opacity-80v text-lg'>{loading ? 'Loading...' :'Sign In'}</button>
@@ -50,7 +50,7 @@ const Signin = () => {
           <span className='text-blue-500'>Sign up</span>
         </Link>
       </div>
-      <p className="text-red-700 mt-5">{error && 'Something went wrong!'}</p>
+      <p className="text-red-700 mt-5">{error ? error.message || 'Something went wrong!' : ""}</p>
     </div>
   )
 }
